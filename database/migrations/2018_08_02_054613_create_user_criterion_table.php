a18b958b80023c79ab7ec5f0af51cb50a7699c37
Louis-Henri Merino <louishenrimerino@hotmail.com>
2018-08-26 13:09:45 -0400

    Migration Update - Remember Token nullable field - Fixed Bug for user_criterion_table

--- database/migrations/2018_08_02_054613_create_user_criterion_table.php
+++ database/migrations/2018_08_02_054613_create_user_criterion_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserCriteriaTable extends Migration
+class CreateUserCriterionTable extends Migration
 {
     /**
      * Run the migrations.
